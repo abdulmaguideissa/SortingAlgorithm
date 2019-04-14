39cccc7034bff1d60ccea8403e600a9cf03ff006
Abdulmaguid Eissa <abdulmaguid.eissa@gmail.com>
2019-04-14 15:02:46 +0200

    Add files via upload

---
+++ MergeSort/msort.h
@@ -0,0 +1,41 @@
+/** @file 
+    msort.h
+    
+  * @brief description
+    API to interface the implementation of merger sort algorithm.
+
+  * @author
+    Abdulmaguid Eissa.
+
+  * @date
+    January, 2019
+
+  *	@COPYRIGHT NOTICE 
+  *
+  *
+  *
+* */
+
+
+#ifndef MERGESORT__H
+#define MERGESORT__H
+
+
+// ************* merge_sort **************
+// brief description: implementation of merge sort algorithm using C.
+//                    divide and conquer algorithm.
+// inputs:  pointer to array of integers,
+//          the begining of the array,
+//          the end of the array.
+// outputs: none
+// NOTES:
+void
+merge_sort(int32_t *arr, uint32_t beg, uint32_t end);
+
+
+
+
+
+#endif  // ************* MERGESORT__H *************
+
+        // ************* end of file *************
